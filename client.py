6d5eaab3bbb4bf6785eb1c66a1e5f4736411cf39
Mayank Maheshwari <Mayankmaheshwari3333@gmail.com>
2019-11-10 18:33:35 +0530

    updated client side

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ def sendData():
     PORT = input()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, int(PORT)))
-        s.sendall('Hello, world')
+        s.sendall(b'Hello, world')
         data = s.recv(1024)
 
     print('Received', repr(data))
