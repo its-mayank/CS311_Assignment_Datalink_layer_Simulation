527641110d4f17f6b70f882ddae0938d3c4effc2
Mayank Maheshwari <Mayankmaheshwari3333@gmail.com>
2019-11-10 15:05:04 +0530

    updated the variable and function name and modulated the code

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ from bitarray import bitarray
 
 
 def xor(a, b): 
-  
-    # initialize result 
     result = [] 
-  
-    # Traverse all bits, if bits are 
-    # same, then XOR is 0, else 1 
     for i in range(1, len(b)): 
         if a[i] == b[i]: 
             result.append('0') 
@@ -18,36 +13,19 @@ def xor(a, b):
     return ''.join(result)
 
 
-def mod2div(divident, divisor): 
-  
-    # Number of bits to be XORed at a time. 
-    pick = len(divisor) 
-  
-    # Slicing the divident to appropriate 
-    # length for particular step 
+def modulus_2_division(divident, divisor): 
+    pick = len(divisor)  
     tmp = divident[0 : pick] 
   
     while pick < len(divident): 
   
         if tmp[0] == '1': 
-  
-            # replace the divident by the result 
-            # of XOR and pull 1 bit down 
             tmp = xor(divisor, tmp) + divident[pick] 
   
-        else:   # If leftmost bit is '0' 
-            # If the leftmost bit of the dividend (or the 
-            # part used in each step) is 0, the step cannot 
-            # use the regular divisor; we need to use an 
-            # all-0s divisor. 
+        else:
             tmp = xor('0'*pick, tmp) + divident[pick] 
-  
-        # increment pick to move further 
         pick += 1
-  
-    # For the last n bits, we have to carry it out 
-    # normally as increased value of pick will cause 
-    # Index Out of Bounds. 
+
     if tmp[0] == '1': 
         tmp = xor(divisor, tmp) 
     else: 
@@ -64,28 +42,25 @@ def bit_to_int(x):
     return m
 
 
-def crcincode():
+def crcencode():
     bits="1"
     m="10101"
     r="0000"
     bits+=r
-    div=int(0)
-    divd=int(0)
-    for i in bits:
-        div=2*div+int(i)
-        
-    for i in m:
-        divd=2*divd+int(i)
+    div = 0
+    divd = 0
+    div = bit_to_int(bits)
 
+    divd = bit_to_int(m)
 
-    rem=mod2div(bits,m)
+    rem=modulus_2_division(bits,m)
     reminder=bit_to_int(rem)
     msg=bit_to_int(bits)
     return np.binary_repr(msg+reminder+1)
 
 
 def crcdecode(msg):
-    if int(mod2div(msg,"10101"))==0:
+    if int(modulus_2_division(msg,"10101"))==0:
         print("NO ERROR\nMessage is",msg)
     else:
         print("ERROR DETECTED")
